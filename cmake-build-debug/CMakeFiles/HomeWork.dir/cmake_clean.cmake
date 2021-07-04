478fa9680f2d5950cd4abc813f1432c7853b6893
unknown <140623yy@163.com>
2021-07-04 15:18:01 +0800

    Created HomeWork

---
+++ cmake-build-debug/CMakeFiles/HomeWork.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HomeWork.dir/main.c.obj"
+  "HomeWork.exe"
+  "HomeWork.exe.manifest"
+  "HomeWork.pdb"
+  "libHomeWork.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/HomeWork.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
